44da7f6282e623566d0a85e07a8829e75aad30bf
1Seven3Seven <72650065+1Seven3Seven@users.noreply.github.com>
2024-05-26 21:40:13 +0930

    An example of how to run this.

---
+++ commands/example.sh
@@ -0,0 +1,1 @@
+python main.py --load-data-file example_load_data.py --paraphraser-file example_paraphraser.py --paraphraser-conda-env TopicsBase --ai-detector-file example_detector.py --ai-detector-conda-env TopicsBase
